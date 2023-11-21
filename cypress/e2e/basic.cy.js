d0e47274a1f371dbf40dfb9f3facbd5bfb2ebdf9
Daniel Gehlen <harmonia251251@gmail.com>
2023-11-21 15:07:03 -0300

    Commit

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,7 +5,7 @@ describe('sample render test', () => {
 
   it('displays the title text', () => {
     cy.log('Before get');
-    cy.get('h1').contains('Next.js Blog Theme', { timeout: 10000 }); // Timeout de 10 segundos
+    cy.get('h1', { timeout: 20000 }).should('contain', 'Next.js Blog Theme');
     cy.log('After get');
   });
 });
